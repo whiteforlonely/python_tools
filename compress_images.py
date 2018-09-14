a8547b96fd9c7e4c5f3b2b98c9629d3fd8cd7725
keqingyuan <qingyuan_888930@sina.com>
2018-09-14 17:23:58 +0800

    can compress any png file in any directory

--- compress_images.py
+++ compress_images.py
@@ -1,16 +1,27 @@
 import os
+import sys
 
 
 # compress the specific directory png files.
-def compress_png_files(dirName):
-	files = os.listdir(dirName)
-	for fileName in files:
-		if not os.path.isdir(fileName):
-			# start to compress the png file
-			if fileName.endswith('.png'):
-				os.system("pngquant --force --output {0} --posterize 4 --skip-if-larger {0}".format(fileName))
-		else:
-			compress_png_files("{}/{}".format(dirName, fileName))
+def compress_png_files(dir_name):
+    files = os.listdir(dir_name)
+    for file_name in files:
+        if not os.path.isdir(file_name):
+            if file_name.endswith(".png"):
+                os.system("pngquant --force --output {0} --posterize 4 --skip-if-larger {0}".format(file_name))
+        else:
+            compress_png_files("{}/{}".format(dir_name, file_name))
 
 
-compress_png_files('.')
+arg_len = 0
+# make sure there is one argument
+for argv in sys.argv:
+    arg_len += 1
+else:
+    if arg_len <= 1:
+        print("compress current directory images...")
+        compress_png_files(".")
+    else:
+        compress_path = sys.argv[1]
+        if not compress_path:
+            compress_png_files(compress_path)
