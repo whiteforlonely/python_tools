12599f8c65c0edd2897e114c53725154e433ff42
keqingyuan <qingyuan_888930@sina.com>
2018-09-14 18:12:10 +0800

    fix bugs

--- compress_images.py
+++ compress_images.py
@@ -6,9 +6,14 @@ import sys
 def compress_png_files(dir_name):
     files = os.listdir(dir_name)
     for file_name in files:
+        # here should direct the file path
+        file_name = "{}/{}".format(dir_name, file_name)
         if not os.path.isdir(file_name):
+            # print("come to file is not dir")
             if file_name.endswith(".png"):
+                # print("{} is png file".format(file_name))
                 os.system("pngquant --force --output {0} --posterize 4 --skip-if-larger {0}".format(file_name))
+                print("compress png: {} ok.".format(file_name))
         else:
             compress_png_files("{}/{}".format(dir_name, file_name))
 
@@ -23,5 +28,6 @@ else:
         compress_png_files(".")
     else:
         compress_path = sys.argv[1]
-        if not compress_path:
+        print("the argument is {}. start to compress path".format(compress_path))
+        if compress_path:
             compress_png_files(compress_path)
